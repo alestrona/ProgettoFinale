5f2c3dfe21642fe50fdf487264a784bd7556a3ac
alestrona <stronati.alessandro@iisgalvanimi.edu.it>
2019-05-21 09:22:15 +0000

    primocommit

--- Monopattini/src/app/app.module.ts
+++ Monopattini/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component';
@@ -12,7 +12,7 @@ import { SingupComponent } from './singup/singup.component';
     SingupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
